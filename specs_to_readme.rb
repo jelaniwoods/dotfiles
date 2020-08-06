e49ff9b8f367a4accc138d485610ed06dca7b970
Jelani Woods <jelani@firstdraft.com>
2020-08-06 17:20:13 -0500

    Update specs_to_readme.rb

--- specs_to_readme.rb
+++ specs_to_readme.rb
@@ -1,13 +1,22 @@
+  
 #!/usr/bin/env ruby
 `rspec --order default --require spec_helper --format documentation --no-color --out specs.md`
-# specs = open("specs.md").read
-# cleanup specs variable somehow
-# Add 
+specs = open("specs.md").read
 
+new_spec_content = specs.gsub(/Failures:(.|\n)*/, "").gsub(/\(FAILED - \d\)/, "")
+new_spec_content = new_spec_content.gsub(/$\n^\s\s/, " ")
 ## Specs
+top = <<~HEREDOC
+<details>
+  <summary>Click here to see names of each test</summary>
+HEREDOC
+readme = File.open("README.md", "a")
+readme.write("\n")
+readme.write("## Specs")
+readme.write("\n")
+readme.write(top)
+readme.write(new_spec_content)
+readme.write("</details>")
+readme.close
 
-##<details>
-##<summary>Click here to see names of each test</summary>
-# to bottom of README
-#then specs
-## then </details>
+File.delete("specs.md")
