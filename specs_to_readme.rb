3bafa8b44dfab0730b91b9853f423e98edaf8c4e
Jelani Woods <jelani@firstdraft.com>
2020-08-06 09:26:48 -0500

    Create specs_to_readme.rb

---
+++ specs_to_readme.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+`rspec --order default --require spec_helper --format documentation --no-color --out specs.md`
+# specs = open("specs.md").read
+# cleanup specs variable somehow
+# Add 
+
+## Specs
+
+##<details>
+##<summary>Click here to see names of each test</summary>
+# to bottom of README
+#then specs
+## then </details>
